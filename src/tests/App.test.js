f04d6cb79d1a1ccb16036d6b741cc79d82f3d5b7
Luis Fernandes Neto <luisfernandesneto@gmail.com>
2022-09-21 19:20:33 -0300

    100% testes

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -68,4 +68,70 @@ describe('Testanto tudo', () => {
 
     expect(screen.getAllByRole('row')).toHaveLength(2);
   });
+  test('testando botões de excluir', async () => {
+    jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(testData),
+    });
+    render(<App />);
+    await waitFor(() => expect(global.fetch).toHaveBeenCalled());
+
+    const columnSelect = screen.getByTestId('column-filter');
+    const comparisonSelect = screen.getByTestId('comparison-filter');
+    const valueinput = screen.getByTestId('value-filter');
+    const buttonElement = screen.getByRole('button', { name: /filtrar/i });
+
+    userEvent.selectOptions(columnSelect, 'orbital_period');
+    userEvent.selectOptions(comparisonSelect, 'maior que');
+    userEvent.type(valueinput, '500');
+    userEvent.click(buttonElement);
+
+    const deleteButton = screen.getByRole('button', { name: /excluir/i });
+    expect(deleteButton).toBeInTheDocument();
+
+    expect(screen.getAllByRole('row')).toHaveLength(four);
+
+    userEvent.click(deleteButton);
+    expect(screen.getAllByRole('row')).toHaveLength(eleven);
+  });
+  test('testando botão de excluir todos filtros', async () => {
+    jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(testData),
+    });
+    render(<App />);
+    await waitFor(() => expect(global.fetch).toHaveBeenCalled());
+
+    const columnSelect = screen.getByTestId('column-filter');
+    const comparisonSelect = screen.getByTestId('comparison-filter');
+    const valueinput = screen.getByTestId('value-filter');
+    const buttonElement = screen.getByRole('button', { name: /filtrar/i });
+
+    userEvent.selectOptions(columnSelect, 'orbital_period');
+    userEvent.selectOptions(comparisonSelect, 'maior que');
+    userEvent.type(valueinput, '500');
+    userEvent.click(buttonElement);
+
+    expect(screen.getAllByRole('row')).toHaveLength(four);
+
+    userEvent.selectOptions(columnSelect, 'diameter');
+    userEvent.selectOptions(comparisonSelect, 'menor que');
+    userEvent.type(valueinput, '11000');
+    userEvent.click(buttonElement);
+
+    expect(screen.getAllByRole('row')).toHaveLength(three);
+
+    userEvent.selectOptions(columnSelect, 'rotation_period');
+    userEvent.selectOptions(comparisonSelect, 'igual a');
+    userEvent.type(valueinput, '24');
+    userEvent.click(buttonElement);
+
+    expect(screen.getAllByRole('row')).toHaveLength(2);
+
+    const deleteAllButton = screen.getByRole('button', { name: /remover todas/i });
+    expect(deleteAllButton).toBeInTheDocument();
+
+    userEvent.click(deleteAllButton);
+    expect(screen.getAllByRole('row')).toHaveLength(eleven);
+  });
 });
