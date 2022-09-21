96a3308a21d41927da5d6e3a9009f4f0e52d561b
Luis Fernandes Neto <luisfernandesneto@gmail.com>
2022-09-21 17:49:02 -0300

    Requisito 6

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
 test('I am your test', () => {
   render(<App />);
   const linkElement = screen.getByRole('heading', { name: /filter by name/i, level: 5 });
   expect(linkElement).toBeInTheDocument();
+
+  const inputElement = screen.getByTestId('name-filter');
+  expect(inputElement).toBeInTheDocument();
+
+  userEvent.type(inputElement, 'Tatooine');
 });
